9caa4fcb1f43be348f8c5d100a36c26fec88eb5d
Conner Wolfe <37260117+cbwolfe94@users.noreply.github.com>
2022-02-13 18:01:54 -0500

    Delete build directory

--- Hello World/build-HelloWorld-Desktop-Debug/CMakeFiles/HelloWorld.dir/HelloWorld_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/HelloWorld.dir/HelloWorld_autogen/mocs_compilation.cpp.o: \
- /home/conner/Documents/qt/build-HelloWorld-Desktop-Debug/HelloWorld_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
